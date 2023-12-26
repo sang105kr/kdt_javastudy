c6c0fd7e0b09f1d88bcf7496897711a0f05079b3
sanghak <sang105kr@gmail.com>
2023-12-26 12:50:58 +0900

    20231226-4

--- src/main/java/com/kh/day5/Method1.java
+++ src/main/java/com/kh/day5/Method1.java
@@ -7,8 +7,10 @@ package com.kh.day5;
 
     메소드 :
         - 실행문을 의미있게 묶는 단위.
-        - 선언부(반환타입+메소드이름+매개변수) + 본문
+        - 선언부(제어자+반환타입(void, 기본형, 참조형)+메소드이름+매개변수) + 본문
         - 메소드 마지막에 return문이 존재한다 (반환타입이 void면 생략가능)
+
+    void : 메소드를 호출한곳에 반환할값이 없음을 뜻함.
  */
 public class Method1 {
     public static void main(String[] args) {
---
+++ src/main/java/com/kh/day5/Method2.java
@@ -0,0 +1,48 @@
+package com.kh.day5;
+/*
+    표현식(4) : 값(리터럴), 변수, 식, 메소드호출
+    지역 변수 : 메소드 내에 선언된 변수
+    매개 변수(parameter) : 메소드 호출될때 입력받는 값으로 초기화되는 변수로
+                          메소드 선언부에서 지역변수로 사용된다.
+
+    인수(argument) : 메소드 호출할때 전달하는 값.
+ */
+public class Method2 {
+    public static void main(String[] args) {
+
+        method1();     // 반환타입(X), 매개변수(X)
+
+        int result2 = method2();  // 반환타입(O), 매개변수(X)
+        System.out.println("result2 = " + result2);
+
+        method3(2); // 반환타입(X), 매개변수(O)
+
+        int result4 = method4(10, 20);
+        System.out.println("result4 = " + result4);
+    }
+
+    // 반환타입(X), 매개변수(X)
+    public static void method1(){
+        System.out.println("method1()");
+
+    }
+    // 반환타입(O), 매개변수(X)
+    public static int method2(){
+        int x = 10;
+        int y = 20;
+        return x + y;
+    }
+
+    // 반환타입(X), 매개변수(O)
+    public static void method3(int x){
+        for (int i = 0; i < x; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    // 반환타입(O), 매개변수(O)
+    public static int method4(int x, int y){
+
+        return x + y;
+    }
+}
