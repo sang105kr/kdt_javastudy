bb26c815d4978b3ff6d0bb347e92abe0be34b8d2
sanghak <sang105kr@gmail.com>
2024-01-05 10:32:44 +0900

    20240105-2-2

--- src/main/java/com/kh/day11/q11/Complex.java
+++ src/main/java/com/kh/day11/q11/Complex.java
@@ -1,2 +1,38 @@
-package com.kh.day11.q11;public class Complex {
+package com.kh.day11.q11;
+
+import com.kh.day10.excercise12.q3.C;
+
+public class Complex {
+    private double realPart;        // 실수부의 값
+    private double imaginaryPart;   // 허수부의 값
+
+    public Complex() {
+    }
+
+    public Complex(double realPart, double imaginaryPart) {
+        this.realPart = realPart;
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public void print() {
+        System.out.println("(" + realPart + "," + imaginaryPart + ")");
+    }
+
+    public double getRealPart() {
+        return realPart;
+    }
+
+    public double getImaginaryPart() {
+        return imaginaryPart;
+    }
+
+    public Complex substract(Complex complex1, Complex complex2){
+        Complex result = null;
+
+        double realPart = complex1.getRealPart() - complex2.getRealPart();
+        double imaginaryPart = complex1.getImaginaryPart() - complex2.getImaginaryPart();
+        result = new Complex(realPart,imaginaryPart);
+
+        return result;
+    }
 }
--- src/main/java/com/kh/day11/q11/ComplexDriver.java
+++ src/main/java/com/kh/day11/q11/ComplexDriver.java
@@ -1,2 +1,20 @@
-package com.kh.day11.q11;public class ComplexDriver {
+package com.kh.day11.q11;
+
+public class ComplexDriver {
+    public static void main(String[] args) {
+//        1.	2 + 3i를 나타내는 복소수 객체 x를 생성한다.
+        Complex x = new Complex(2, 3);
+//        2.	-3 + 7i를 나타내는 복소수 객체 y를 생성한다.
+        Complex y = new Complex(-3, 7);
+
+//        3.	z = x – y
+        Complex z = new Complex();
+        z = z.substract(x,y);
+
+//        4.	x, y, z를 출력한다
+        x.print();
+        y.print();
+        z.print();
+
+    }
 }
