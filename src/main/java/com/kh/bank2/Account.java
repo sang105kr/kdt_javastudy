c902e71e93a52070c91c644df00ca8a3a449de72
sanghak <sang105kr@gmail.com>
2024-01-10 11:49:36 +0900

    20240110-3

--- src/main/java/com/kh/bank2/Account.java
+++ src/main/java/com/kh/bank2/Account.java
@@ -26,9 +26,12 @@ public class Account {
         for (int i = 0; i < accounts.length; i++) {
             if (accounts[i] == null) {
                 accounts[i] = this;
-                break;
+                System.out.println("신규 계좌와 아래와 같이 생성되었습니다.");
+                System.out.println(this);
+                return;
             }
         }
+        System.out.println("계좌 개설 한도수("+ACCOUNTS_MAX_SIZE+")가 모두 찼습니다.");
     }
 
     // 계좌번호 생성
@@ -40,19 +43,93 @@ public class Account {
         }
         return str;
     }
+    //폐지
+    public void closingAccount(String accountNumber){
+        //계좌번호로 계좌 찾아오기
+        int idx = findAccount(accountNumber);
+        if(idx == -1){
+            System.out.println("찾고자하는 계좌가 없습니다.");
+            return;
+        }
+        Account account = accounts[idx];
+
+        //잔액이 남아 있는 경우 폐지 불가능 하다.
+        if (account.balance > 0) {
+            System.out.println("잔액이 존재합니다");
+            return;
+        }
+        
+        //폐지 처리
+        account = null;
+    }
+
 
     //입금
     public void deposit(String accountNumber, int money) {
-        balance += money;
+        //1회 입금한도는 4만원을 초과할 수 없다.
+        if(money > 40_000){
+            System.out.println("1회 입금한도는 4만원을 초과할 수 없다.");
+            return;
+        }
+
+        //계좌번호로 계좌 찾아오기
+        int idx = findAccount(accountNumber);
+        if(idx == -1){
+            System.out.println("찾고자하는 계좌가 없습니다.");
+            return;
+        }
+        Account account = accounts[idx];
+
+        //예치금액은 10만원 이하로 제한 한다.
+        if(account.balance + money > 100_000){
+            System.out.println("예치금액은 10만원 이하로 제한 한다.");
+            return;
+        }
+
+        //입금처리
+        account.balance += money;
+        System.out.println(account.toString());
     }
     //출금
     public void widthdraw(String accountNumber, int money) {
-        balance -= money;
+        //1회 출금한도는 4만원을 초과할 수 없다.
+        if(money > 40_000){
+            System.out.println("1회 출금한도는 4만원을 초과할 수 없다.");
+            return;
+        }
+
+        //계좌번호로 계좌 찾아오기
+        int idx = findAccount(accountNumber);
+        if(idx == -1){
+            System.out.println("찾고자하는 계좌가 없습니다.");
+            return;
+        }
+        Account account = accounts[idx];
+
+        //마이너스 잔고는 허용하지 않는다.
+        if(account.balance - money < 0){
+            System.out.println("잔액이 부족합니다.");
+            return;
+        }
+
+        //출금처리
+        account.balance -= money;
+        System.out.println(account);
     }
 
     //조회(개별)
     public void getAccount(String accountNumber) {
-
+        //계좌번호로 계좌 찾아오기
+        int idx = findAccount(accountNumber);
+        if(idx == -1){
+            System.out.println("찾고자하는 계좌가 없습니다.");
+            return;
+        }
+        Account account = accounts[idx];
+        System.out.print("계좌번호 : " + account.accountNumber);
+        System.out.print("\t예금주명 : " + account.accountName);
+        System.out.print("\t잔액 : " + account.balance);
+        System.out.println();
     }
 
     //조회(전체)
@@ -71,5 +148,23 @@ public class Account {
         }
     }
 
+    //계좌 검색
+    private int findAccount(String accountNumber) {
+        int idx = -1;
+        for (int i = 0; i < accounts.length; i++) {
+            if(accounts[i].accountNumber.equals(accountNumber)){
+                idx = i;
+                break;
+            }
+        }
 
+        return idx;
+    }
+
+    @Override
+    public String toString() {
+        return  "계좌번호 : " + accountNumber +
+                "\t 예금주명 : " + accountName +
+                "\t 잔액 : " + balance + "\n";
+    }
 }
--- src/main/java/com/kh/bank2/AccountMain.java
+++ src/main/java/com/kh/bank2/AccountMain.java
@@ -6,7 +6,10 @@ public class AccountMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean stop = false;
-        Account account = null;
+        Account account = null;         //계좌
+        String accountName = null;      //예금주명
+        String accountNumber = null;    //계좌번호
+        int money = 0;                  //입출금액
         while (!stop) {
             System.out.println("1.신규 2.폐지 3.입금 4.출금 5.계좌조회(개별) 6.계좌조회(전체) 7.종료");
             System.out.print("선택 >> ");
@@ -15,16 +18,32 @@ public class AccountMain {
             switch (menu) {
                 case 1: //신규
                     System.out.print("예금주명 : ");
-                    String accountName = scanner.nextLine();
+                    accountName = scanner.nextLine();
                     account = new Account(accountName);
                     break;
-                case 2:
+                case 2: //폐지
+                    System.out.print("계좌번호 : ");
+                    accountNumber = scanner.nextLine();
+                    account.closingAccount(accountNumber);
                     break;
-                case 3:
+                case 3: //입금
+                    System.out.print("계좌번호 : ");
+                    accountNumber = scanner.nextLine();
+                    System.out.print("입금액 : ");
+                    money = scanner.nextInt();
+                    account.deposit(accountNumber,money);
                     break;
-                case 4:
+                case 4: //출금
+                    System.out.print("계좌번호 : ");
+                    accountNumber = scanner.nextLine();
+                    System.out.print("출금액 : ");
+                    money = scanner.nextInt();
+                    account.widthdraw(accountNumber,money);
                     break;
-                case 5:
+                case 5: // 개별조회
+                    System.out.print("계좌번호 : ");
+                    accountNumber = scanner.nextLine();
+                    account.getAccount(accountNumber);
                     break;
                 case 6: //전체 조회
                     account.listAccount();
